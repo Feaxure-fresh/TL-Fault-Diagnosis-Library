d69da926dd683f3c0099e44e47e1e6efc31d83af
Feaxure <feaxure@outlook.com>
2022-04-22 11:04:21 +0800

    date 4.22

---
+++ utils.py
@@ -0,0 +1,227 @@
+import torch
+import logging
+import numpy as np
+from torch import nn
+from torch.autograd import Function
+
+
+def setlogger(path):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logFormatter = logging.Formatter("%(asctime)s %(message)s", "%m-%d %H:%M:%S")
+
+    fileHandler = logging.FileHandler(path)
+    fileHandler.setFormatter(logFormatter)
+    logger.addHandler(fileHandler)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    logger.addHandler(consoleHandler)
+
+
+def get_accuracy(preds, targets):
+        assert preds.shape[0] == targets.shape[0]
+        correct = torch.eq(preds.argmax(dim=1), targets).float().sum().item()
+        accuracy = correct/preds.shape[0]
+        
+        return accuracy
+
+
+def gmean(iterable):
+    a = np.array(iterable)
+    return a.prod() ** (1. / len(a))
+
+
+def freeze_net(net):
+    if not net:
+        return
+    for p in net.parameters():
+        p.requires_grad = False
+
+
+def unfreeze_net(net):
+    if not net:
+        return
+    for p in net.parameters():
+        p.requires_grad = True
+
+
+def get_next_batch(loaders, iters, src, device):
+    inputs, labels = None, None
+    if src == 'all':
+        for key in iters.keys():
+            try:
+                inputs, labels = next(iters[key])
+                break
+            except StopIteration:
+                continue
+        if inputs == None:
+            for key in iters.keys():
+                iters[key] = iter(loaders[key])
+            inputs, labels = next(iters[iters.keys()[0]])
+    else:
+        try:
+            inputs, labels = next(iters[src])
+        except StopIteration:
+            iters[src] = iter(loaders[src])
+            inputs, labels = next(iters[src])
+
+    return inputs.to(device), labels.to(device)
+
+
+def get_gate_label(gate_out, idx, device):
+    labels = torch.full(gate_out.size()[:-1], idx, dtype=torch.long)
+    labels = labels.to(device)
+    return labels
+
+
+def one_hot(x, class_count):
+    return torch.eye(class_count)[x,:]
+
+
+def mfsan_guassian_kernel(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+
+    n_samples = int(source.size()[0])+int(target.size()[0])
+    total = torch.cat([source, target], dim=0)
+    total0 = total.unsqueeze(0).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    total1 = total.unsqueeze(1).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    L2_distance = ((total0-total1)**2).sum(2)
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data) / (n_samples**2-n_samples)
+    bandwidth /= kernel_mul ** (kernel_num // 2)
+    bandwidth_list = [bandwidth * (kernel_mul**i) for i in range(kernel_num)]
+    kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list]
+
+    return sum(kernel_val)
+
+
+def MFSAN_mmd(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None, cb=None):
+
+    batch_size = int(source.size()[0])
+    kernels = mfsan_guassian_kernel(source, target, kernel_mul=kernel_mul, 
+                               kernel_num=kernel_num, fix_sigma=fix_sigma)
+    XX = kernels[:batch_size, :batch_size]
+    YY = kernels[batch_size:, batch_size:]
+    XY = kernels[:batch_size, batch_size:]
+    YX = kernels[batch_size:, :batch_size]
+    if cb != None:
+        loss = torch.mean(XX + cb * cb.T * YY - cb * XY - cb.T * YX)
+    else:
+        loss = torch.mean(XX + YY - XY -YX)
+
+    return loss
+
+
+class GradientReverseFunction(Function):
+
+    @staticmethod
+    def forward(ctx, input, coeff = 1.):
+        ctx.coeff = coeff
+        output = input * 1.0
+
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+
+        return grad_output.neg() * ctx.coeff, None
+
+
+class GradientReverseLayer(nn.Module):
+    def __init__(self):
+        super(GradientReverseLayer, self).__init__()
+
+    def forward(self, *input):
+        return GradientReverseFunction.apply(*input)
+
+
+class WarmStartGradientReverseLayer(nn.Module):
+
+    def __init__(self, alpha = 1.0, lo = 0.0, hi = 1.,
+                      max_iters = 1000., auto_step = False):
+        super(WarmStartGradientReverseLayer, self).__init__()
+        self.alpha = alpha
+        self.lo = lo
+        self.hi = hi
+        self.iter_num = 0
+        self.max_iters = max_iters
+        self.auto_step = auto_step
+
+    def forward(self, input):
+        coeff = np.float(
+            2.0 * (self.hi - self.lo) / (1.0 + np.exp(-self.alpha * self.iter_num / self.max_iters))
+            - (self.hi - self.lo) + self.lo)
+        if self.auto_step:
+            self.step()
+
+        return GradientReverseFunction.apply(input, coeff)
+
+    def step(self):
+        self.iter_num += 1
+
+
+def _update_index_matrix(batch_size, index_matrix = None, linear = True):
+    if index_matrix is None or index_matrix.size(0) != batch_size * 2:
+        index_matrix = torch.zeros(2 * batch_size, 2 * batch_size)
+        if linear:
+            for i in range(batch_size):
+                s1, s2 = i, (i + 1) % batch_size
+                t1, t2 = s1 + batch_size, s2 + batch_size
+                index_matrix[s1, s2] = 1. / float(batch_size)
+                index_matrix[t1, t2] = 1. / float(batch_size)
+                index_matrix[s1, t2] = -1. / float(batch_size)
+                index_matrix[s2, t1] = -1. / float(batch_size)
+        else:
+            for i in range(batch_size):
+                for j in range(batch_size):
+                    if i != j:
+                        index_matrix[i][j] = 1. / float(batch_size * (batch_size - 1))
+                        index_matrix[i + batch_size][j + batch_size] = 1. / float(batch_size * (batch_size - 1))
+            for i in range(batch_size):
+                for j in range(batch_size):
+                    index_matrix[i][j + batch_size] = -1. / float(batch_size * batch_size)
+                    index_matrix[i + batch_size][j] = -1. / float(batch_size * batch_size)
+
+    return index_matrix
+
+
+class MultipleKernelMaximumMeanDiscrepancy(nn.Module):
+
+    def __init__(self, kernels, linear = False):
+        super(MultipleKernelMaximumMeanDiscrepancy, self).__init__()
+        self.kernels = kernels
+        self.index_matrix = None
+        self.linear = linear
+
+    def forward(self, z_s, z_t):
+        features = torch.cat([z_s, z_t], dim=0)
+        batch_size = int(z_s.size(0))
+        self.index_matrix = _update_index_matrix(batch_size, self.index_matrix, self.linear).to(z_s.device)
+
+        # Add up the matrix of each kernel
+        kernel_matrix = sum([kernel(features) for kernel in self.kernels])
+        # Add 2 / (n-1) to make up for the value on the diagonal
+        # to ensure loss is positive in the non-linear version
+        loss = (kernel_matrix * self.index_matrix).sum() + 2. / float(batch_size - 1)
+
+        return loss
+
+
+class GaussianKernel(nn.Module):
+
+    def __init__(self, sigma = None, track_running_stats = True, alpha = 1.):
+        super(GaussianKernel, self).__init__()
+        assert track_running_stats or sigma is not None
+        self.sigma_square = torch.tensor(sigma * sigma) if sigma is not None else None
+        self.track_running_stats = track_running_stats
+        self.alpha = alpha
+
+    def forward(self, X):
+        l2_distance_square = ((X.unsqueeze(0) - X.unsqueeze(1)) ** 2).sum(2)
+
+        if self.track_running_stats:
+            self.sigma_square = self.alpha * torch.mean(l2_distance_square.detach())
+
+        return torch.exp(-l2_distance_square / (2 * self.sigma_square))
