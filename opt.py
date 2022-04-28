a0f5076d21613886225311c693eedff666220856
Feaxure <feaxure@outlook.com>
2022-04-28 10:50:18 +0800

    date 4.28

---
+++ opt.py
@@ -0,0 +1,36 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train')
+
+    # basic parameters
+    parser.add_argument('--model_name', type=str, default='DAN', help='the name of the model')
+    parser.add_argument('--source_name', type=list, default=['CWRU_2'], help='the name of the source data')
+    parser.add_argument('--target_name', type=str, default='CWRU_3', help='the name of the target data')
+    parser.add_argument('--data_dir', type=str, default="./dataset", help='the directory of the data')
+    parser.add_argument('--train_mode', type=str, choices=['single_source', 'source_combine', 'multi_source'],
+                        default="single_source", help='the mode for training')
+    parser.add_argument('--normlizetype', type=str, choices=['0-1', '-1-1', 'mean-std'], default='-1-1', help='data normalization methods')
+    parser.add_argument('--cuda_device', type=str, default='0', help='assign device')
+    parser.add_argument('--checkpoint_dir', type=str, default='./Ckpt/CWRU', help='the directory to save the model')
+    parser.add_argument('--batch_size', type=int, default=64, help='batchsize of the training process')
+    parser.add_argument('--num_workers', type=int, default=1, help='the number of training process')
+    parser.add_argument('--num_classes', type=int, default=9, help='the classes of data')
+
+    # optimization information
+    parser.add_argument('--opt', type=str, choices=['sgd', 'adam'], default='sgd', help='the optimizer')
+    parser.add_argument('--lr', type=float, default=0.01, help='the initial learning rate')
+    parser.add_argument('--momentum', type=float, default=0.9, help='the momentum for sgd')
+    parser.add_argument('--weight_decay', type=float, default=5e-4, help='the weight decay')
+    parser.add_argument('--lr_scheduler', type=str, choices=['step', 'exp', 'stepLR', 'fix'], default='fix', help='the learning rate schedule')
+    parser.add_argument('--gamma', type=float, default=0.2, help='learning rate scheduler parameter for step and exp')
+    parser.add_argument('--tradeoff', type=list, default=['exp', 'exp', 'exp'], help='coefficients of loss')
+    parser.add_argument('--dropout', type=list, default=0, help='coefficient of dropout layers')
+    parser.add_argument('--steps', type=str, default='10', help='the learning rate decay for step and stepLR')
+
+    # save, load and display information
+    parser.add_argument('--max_epoch', type=int, default=30, help='max number of epoch')
+    parser.add_argument('--save_step', type=int, default=0, help='the interval of save training model. 0: no saving')
+    args = parser.parse_args()
+    return args
+    
---
+++ train.py
@@ -0,0 +1,42 @@
+import os
+import logging
+from opt import parse_args
+from utils import setlogger
+from datetime import datetime
+from train_utils import train_utils
+
+if __name__ == '__main__': 
+    args = parse_args()
+    if not args.cuda_device:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.cuda_device.strip()
+    if args.train_mode == 'single_source':
+        assert len(args.source_name) == 1
+    else:
+        assert len(args.source_name) > 1
+    
+    # prepare the saving path for the model
+    sub_dir = args.model_name + '_' + str(args.source_name) + 'To' + \
+            args.target_name + '_' + datetime.strftime(datetime.now(), '%m%d-%H%M%S')
+    save_dir = os.path.join(args.checkpoint_dir, sub_dir)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    # set the logger
+    setlogger(os.path.join(save_dir, 'training.log'))
+
+    # save the args
+    for k, v in args.__dict__.items():
+        logging.info("{}: {}".format(k, v))
+    
+    # training
+    trainer = train_utils(args, save_dir)
+    if args.model_name == 'Man_Moe':
+    	trainer.train_man_moe()
+    elif args.model_name == 'MSTLN':
+        trainer.train_mstln()
+    else:
+        if args.train_mode != 'multi_source':
+            trainer.train_1src_and_srccomb()
+        else:
+            trainer.train_multi_src()
+        
