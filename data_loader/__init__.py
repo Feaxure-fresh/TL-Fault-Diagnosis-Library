9c864148dcd0677b8fddb2c2932b6fe897dd442d
Feaxure <feaxure@outlook.com>
2022-04-28 10:48:00 +0800

    date 4.28

---
+++ data_loader/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys  
+sys.path.append('./data_loader')
+from data_loader.MFPT import MFPT
+from data_loader.CWRU import CWRU
+from data_loader.PU import PU
+from data_loader.XJTU import XJTU
